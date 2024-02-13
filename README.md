bcaf242c7a4f7eefcb9f4d01f2d58fe84967ca1b
pradip-p <thapapradip542@gmail.com>
2024-02-13 08:59:59 +0545

    initial code

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
-# indeed_jobs_scrapper
+# Multi-Site Job Scraper with Scrapy
+
+This JobScraper is designed to scrape job listings from multiple sites, including Seek, Indeed, and LinkedIn.
+
+## Requirements
+
+- Python 3.x
+- Scrapy
+- Other dependencies (listed in `requirements.txt`)
+
+## Installation
+
+1. Clone the repository:
+
+    ```bash
+    git clone git@github.com:curaproject/job-scraper.git
+    ```
+
+2. Navigate to the project directory:
+
+    ```bash
+    cd multi-site-scraper
+    ```
+
+3. Create and activate a virtual environment (optional but recommended):
+
+    ```bash
+    python -m venv venv
+    source venv/bin/activate  # On Windows, use `venv\Scripts\activate`
+    ```
+
+4. Install dependencies:
+
+    ```bash
+    pip install -r requirements.txt
+    ```
+
+## Adding Job Categories in google cloud function (Job_crawler)
+
+To add a job category, simply append the existing job categories separated by commas in runtime envirement. Afterward, deploy the function to make the changes effective.
+
+## Usage
+
+To run the scraper and collect job listings from all three sites, execute the following command in local:
+
+```bash
+python main.py
+```
--- main.py
+++ main.py
@@ -1,40 +1,21 @@
-from multiprocessing import Process, Queue
-from job_scraper.spiders import indeed_s
-from scrapy.crawler import CrawlerProcess
-from scrapy.utils.project import get_project_settings
+# import functions_framework
+from indeed import IndeedScraper
 
 
-def indeed(event, context):
-    def script(queue):
-        try:
-            settings = get_project_settings()
 
-            settings.setdict({
-                'LOG_LEVEL': 'ERROR',
-                'LOG_ENABLED': True,
-            })
-
-            process = CrawlerProcess(settings)
-            process.crawl(indeed_s.IndeedSpider)
-            process.start()
-            queue.put(None)
-        except Exception as e:
-            queue.put(e)
-
-    queue = Queue()
-
-    # wrap the spider in a child process
-    main_process = Process(target=script, args=(queue,))
-    main_process.start()    # start the process
-    main_process.join()     # block until the spider finishes
-
-    result = queue.get()    # check the process did not return an error
-    if result is not None:
-        raise result
-        
-    return "ok"
+# @functions_framework.http
+# def indeed_scraper(request):
+def indeed_crawler(event, context):
+    # Create a CrawlerRunner with your project settings
+    collection_name = 'nurse'
+    # Create an instance of the scraper
+    scraper = IndeedScraper(collection_name)
+    # Run the scraper to save the scraped data into MongoDB
+    scraper.run()
+    print('Successfully scraped.....')
 
 
-if __name__ == "__indeed__":
-    indeed("event","context")
-indeed("event","context")
+
+# Uncomment the following lines if running this code outside of a Cloud Function environment
+if __name__ == "__main__":
+    indeed_crawler('','')
